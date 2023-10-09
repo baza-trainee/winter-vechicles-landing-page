5b7a16e1b3dcb60f45f206c2eb6cd2afe787f6db
Baza-trainee <stanislav.gusiev@gmail.com>
2023-10-09 17:14:33 +0300

    feat: add fonts & bag-fix containet style

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
-import './Index.module.scss';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
