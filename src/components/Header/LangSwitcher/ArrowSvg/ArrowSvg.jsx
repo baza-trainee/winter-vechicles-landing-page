5a4adfe99304207774000a81b0630bb599f46d91
Baza-trainee <stanislav.gusiev@gmail.com>
2023-10-12 00:19:07 +0300

    fix: container and style
    1. fix containet component
    2. delete console error

--- src/components/Header/LangSwitcher/ArrowSvg/ArrowSvg.jsx
+++ src/components/Header/LangSwitcher/ArrowSvg/ArrowSvg.jsx
@@ -1,13 +1,13 @@
-import styles from './arrowSvg.module.scss';
+// import styles from './arrowSvg.module.scss';
 
 const ArrowSvg = () => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="14" height="8" fill="none">
       <path
         stroke="#F8F7F7"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
         d="m1 1 6 6 6-6"
       />
     </svg>
