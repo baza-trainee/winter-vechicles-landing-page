9b744bfa1f53bc0da7ddb62b4d0c5222b083dbb1
Baza-trainee <stanislav.gusiev@gmail.com>
2023-10-13 16:58:20 +0300

    fix: imports style.scss in the header component

--- src/components/Header/LogoComponent/LogoComponent.jsx
+++ src/components/Header/LogoComponent/LogoComponent.jsx
@@ -1,4 +1,4 @@
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 import LogoSvg from "./LogoSvg/LogoSvg";
 import styles from "./logoComponent.module.scss";
 
