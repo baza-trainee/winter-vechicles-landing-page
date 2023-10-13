5ddb80e9629bef3eb59f110f14f82bf4b166c6fa
dav1dashka <dujadavyd17@gmail.com>
2023-10-13 15:35:09 +0300

    refactor: refactored code with prettier,
    Also used i18n library, added final animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,40 +1,52 @@
-import Container from "../Container/Container";
-import { BsArrowRight } from "react-icons/bs";
+import { useTranslation } from 'react-i18next';
+import { BsArrowRight } from 'react-icons/bs';
 
-import style from './Hero.module.scss'
+import Container from '../Container/Container';
+
+import style from './Hero.module.scss';
 
 const Hero = () => {
-    return (
-        <>
-            <section className={style.hero}>
-                <Container>
-                    <div className={style.hero__cover}>
-                        <div className={style.hero__info}>
-                            <h1 className={style.hero__title}>Швидкі колеса</h1>
-                            <span className={style.hero__label}>До зими авто підготував - ворогу шансів не дав!</span>
-                            <p className={style.hero__text}>
-                                <span>Наша мета<BsArrowRight preserveAspectRatio="none" className={style.hero__arrow} /></span><span>50 000 грн</span>
-                            </p>
-                            <p className={style.hero__text}>
-                                Для 59 бригади ім. Якова Гандзюка на придбання комплектуючих для авто
-                            </p>
-                        </div>
-                        <div className={style.hero__buttonCover}>
-                            {/* Замінити тег button компонентом Button  */}
-                            <button style={{
-                                'borderRadius': '8px',
-                                'background': 'var(--Orange, #F29544)',
-                                'color': '#F1F1F1',
-                                'padding': '15px 63.5px'
-                            }}>Допомогти</button>
-                        </div>
-                    </div>
-                </Container>
-            </section >
-        </>
-    )
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <section className={style.hero}>
+        <Container>
+          <div className={style.hero__cover}>
+            <div className={style.hero__info}>
+              <h1 className={style.hero__title}>{t('hero.title')}</h1>
+              <span className={style.hero__label}>{t('hero.subcription')}</span>
+              <p className={style.hero__text}>
+                <span>
+                  {t('hero.our-goal')}
+                  <BsArrowRight
+                    preserveAspectRatio="none"
+                    className={style.hero__arrow}
+                  />
+                </span>
+                <span>{t('hero.sum')}</span>
+              </p>
+              <p className={style.hero__text}>{t('hero.for')}</p>
+            </div>
+            <div className={style.hero__buttonCover}>
+              {/* Замінити тег button компонентом Button   */}
+              <button
+                style={{
+                  borderRadius: '8px',
+                  background: 'var(--Orange, #F29544)',
+                  color: '#F1F1F1',
+                  padding: '15px 63.5px',
+                }}
+              >
+                {' '}
+                {t('hero.btn')}
+              </button>
+            </div>
+          </div>
+        </Container>
+      </section>
+    </>
+  );
 };
 
 export default Hero;
-
-
