85b02f1a4c95f11217680b25e94c5e0a94d848fe
Stanilsav Husiev <114510611+Bad-Raider@users.noreply.github.com>
2023-10-14 10:32:56 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,13 @@
 import Container from '../Container/Container';
 import Header from '../Header/Header';
 import Hero from '../Hero/Hero';
-// import AboutBrigade from '../About-brigade/AboutBrigade';
+import AboutBrigade from '../About-brigade/AboutBrigade';
 import Footer from '../Footer/Footer';
 import WhyIsItImportantBlock from '../WhyIsItImportantBlock/WhyIsItImportantBlock';
 import Scroll from '../ScrollToTop/ScrollToTop';
 import RunningText from '../RunningText/RunningText';
 import ProgressBar from '../ProgressBar/ProgressBar';
-// import Modal from '../Modal/Modal';
-
-
+import Modal from '../Modal/Modal';
 import './App.module.scss';
 
 function App() {
@@ -20,17 +18,17 @@ function App() {
         <Hero />
         <RunningText />
         <ProgressBar/>
-        {/* <AboutBrigade /> */}
+        <AboutBrigade /> 
         <RunningText />
         <WhyIsItImportantBlock />
-        <Scroll />
       </main>
       <footer>
         <Container>
           <Footer />
-          {/* <Modal/> */}
         </Container>
       </footer>
+      <Scroll />
+      <Modal/>
     </>
   );
 }
