30f5bb594edb2b60f8b2381dfd65318b6c87c8e9
Veronika Timakova <tymnik@ukr.net>
2023-10-14 19:57:39 +0300

    feat: add button

--- src/components/About-brigade/AboutBrigade.jsx
+++ src/components/About-brigade/AboutBrigade.jsx
@@ -1,4 +1,5 @@
 import { ImgShown } from './imgShown';
+import { HelpButton } from '../Button/HelpButton';
 import { useTranslation } from 'react-i18next';
 import Container from '../Container/Container';
 import styles from './AboutBrigade.module.scss';
@@ -9,16 +10,17 @@ const AboutBrigade = () => {
     <Container>
       <section id="about">
         <div className={styles.container}>
-          <h2 className={styles.sectionTitle}>{t('About.title')}</h2>
+          <h2 className={styles.sectionTitle}>{t('about.title')}</h2>
           <div className={styles.content}>
             <div className={styles.aboutBrigadeText}>
-              <p>{t('About.text-1')}</p>
-              <p>{t('About.text-2')}</p>
+              <p>{t('about.text-1')}</p>
+              <p>{t('about.text-2')}</p>
             </div>
             <div className={styles.img}>
               <ImgShown />
             </div>
           </div>
+          <button>{HelpButton}</button>
         </div>
       </section>
     </Container>
